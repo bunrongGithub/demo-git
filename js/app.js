5e50dabf02215c1144592745a188b9bd767270c6
Rong <bunbunrong993@gmail.com>
2024-12-15 23:45:10 +0700

    js day 01

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-d
+let name = 10 // global scope
+function getName() {
+    name = 'asdfgh';
+    console.log(name);
+}
+getName();
+
+const age = 10; // local scope
+function getAge() {
+    age = 12;
+    console.log(age);
+}
+getAge();
+
+// string , number, boolean ,underfined ,null ,object 
+// ========================
+const arrObject = [
+    {
+        id: 1,
+        name: "asdfgh",
+    },
+    {
+        id: 2,
+        name: "asdfgh",
+    },
+]
+console.log(typeof (arrObject));
+console.log(10);
+let check = false;
+let result = check ? "Check is true" : " Check is false";
+console.log(result);
+
+
+
